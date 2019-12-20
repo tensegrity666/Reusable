de78b0accc01d8e96e5b64d52aef27ff41a4467f
Roman Yakovlev <tensegrity666@eicloud.com>
2019-12-20 22:32:21 +0300

    1-4 completed

--- Exercises/1-let.js
+++ Exercises/1-let.js
@@ -1,5 +1,5 @@
 'use strict';
 
-let name = undefined;
+let name = 'Tensegrity';
 
 module.exports = { name };
--- Exercises/4-range.js
+++ Exercises/4-range.js
@@ -1,5 +1,14 @@
 'use strict';
 
-const range = null;
+const range = (start, end) => {
+  const length = (end - start);
+  if (length < 0) return [];
+  const array = new Array(length);
+  let i = 0;
+  for (let n = start; n <= end; n++) {
+    array[i++] = n;
+  }
+  return array;
+};
 
 module.exports = { range };
